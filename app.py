e9d8685f83c9a99b13c45539253784a9593e2b70
Roshini41412 <250957508+Roshini31412@users.noreply.github.com>
2026-08-04 09:09:47 +0530

    Update greeting message in app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 # app.py
 def main():
-    print("Hello from Jenkins Git Integration Lab!")
+    print("Hello from Jenkins Git Integration Lab ------ TRY 1!")
 
 if __name__ == "__main__":
     main()
